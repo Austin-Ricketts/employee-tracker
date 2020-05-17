650b22bfe81115ae85c8fd6f43a406c886c98edf
Austin Ricketts <austinricketts4@gmail.com>
2020-05-17 14:57:48 -0400

    update employee function is operational

--- app.js
+++ app.js
@@ -38,8 +38,10 @@ connection.connect(function(err) {
           "View all employees",
           "View all roles",
           "View all departments",
+          "Add an employee",
           "Add a role",
-          "Add a department"
+          "Add a department",
+          "Update an employee role"
         ]
       })
       .then(function(answer) {
@@ -56,6 +58,10 @@ connection.connect(function(err) {
           viewDepts();
           break;
         
+        case "Add an employee":
+          addEmps();
+          break;
+        
         case "Add a role":
           addRoles();
           break;
@@ -63,13 +69,17 @@ connection.connect(function(err) {
         case "Add a department":
           addDepts();
           break;
+        
+        case "Update an employee role":
+          confirmUpdate();
+          break;
         }
       });
   }
 
   const viewEmps = () => {
 
-    let query = "SELECT * FROM employee";
+    let query = "SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name AS departmentName FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id;";
     
     connection.query(query, function(err, res) {
         console.log("-------------------------------");
@@ -85,6 +95,96 @@ connection.connect(function(err) {
         else {connection.end();}
     });
   }
+
+  const addEmps = () => {
+    inquirer.prompt([
+        {
+        name: "first",
+        type: "input",
+        message: "What is the first name of the new employee?"
+    },
+    {
+        name: "last",
+        type: "input",
+        message: "What is the last name of the new employee?"
+    },
+    {
+        name: "roleId",
+        type: "input",
+        message: "What is the Role ID number for this new employee?"
+    },
+    {
+        name: "manId",
+        type: "input",
+        message: "What is the Manager ID number for this new employee?"
+    }])
+    .then(function(answer) {
+        connection.query("INSERT INTO employee SET ?", {
+            first_name: answer.first,
+            last_name: answer.last,
+            role_id: answer.roleId,
+            manager_id: answer.manId
+        })
+        viewEmps();
+    });
+}
+
+const confirmUpdate = () => {
+    inquirer.prompt({
+        name: "update",
+        type: "confirm",
+        message: "Would you like to continue with updating an employee?"
+    })
+    .then(function(answer) {
+        if (answer.update === true) {updateEmps();}
+        else {start();}
+    });
+}
+
+const updateEmps = () => {
+    
+            inquirer.prompt([
+                {
+                    name: "id",
+                    type: "input",
+                    message: "What is the employee's original ID?"
+                },
+                {
+                    name: "first",
+                    type: "input",
+                    message: "Update first name"
+                },
+                {
+                    name: "last",
+                    type: "input",
+                    message: "Update last name"
+                },
+                {
+                    name: "roleId",
+                    type: "input",
+                    message: "Update Role ID"
+                },
+                {
+                    name: "manId",
+                    type: "input",
+                    message: "Update Manager ID"
+                }
+            ])
+            .then(function(answer) {
+                connection.query("UPDATE employee SET ? WHERE ?", [
+                    {
+                        first_name: answer.first,
+                        last_name: answer.last,
+                        role_id: answer.roleId,
+                        manager_id: answer.manId
+                    },
+                    {
+                        id: answer.id
+                    }
+                ])
+                viewEmps();
+            })
+}
   
   const viewRoles = () => {
     let query = "SELECT * FROM role";
